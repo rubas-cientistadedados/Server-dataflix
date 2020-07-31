4cc02b076e1fe89cc764be202f61a1c7a2dbac79
Rubas Dev <rubas-dev@protonmail.com>
2020-07-31 00:37:22 -0300

    dependencies adding

---
+++ init.R
@@ -0,0 +1,13 @@
+
+
+# R install packages 
+
+my_packages = c("rjson")
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
